5292579b2e030e49299b6736f346911f241c7dfc
celal <cgorgun>
2017-10-04 14:23:37 -0500

    add username label

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <html>
  <head><title> PHP Test </title></head>
  <body>
- 	<?php echo '<p>Hello World</p>';?>
+ 	<?php echo '<p> Hello World </p>';?>
  	         <form>
                     <label for="username">Username:</label>
                     <input type="text" name="username" id="username">
